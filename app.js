d7e54e90af6be9a6261b096d238245790522f52b
juniorpb <juniorfoxld@hotmail.com>
2020-04-12 23:55:07 -0300

    dark mode

--- app.js
+++ app.js
@@ -57,3 +57,44 @@ function changeHeight() {
 }
 
 window.addEventListener('resize', changeHeight);
+
+// Dark Mode
+const html = document.querySelector("html")
+const checkbox = document.querySelector("input[name=theme]")
+
+const getStyle = (element, style) =>
+    window
+    .getComputedStyle(element)
+    .getPropertyValue(style)
+
+
+const initialColors = {
+    bg: getStyle(html, "--bg"),
+    bgPanel: getStyle(html, "--bg-panel"),
+    colorHeadings: getStyle(html, "--color-headings"),
+    colorText: getStyle(html, "--color-text"),
+}
+
+const darkMode = {
+    bg: "#565656",
+    bgPanel: "#434343",
+    colorHeadings: "#3664FF",
+    colorText: "#fbfbfb"
+}
+
+const transformKey = key =>
+    "--" + key.replace(/([A-Z])/, "-$1").toLowerCase()
+
+
+const changeColors = (colors) => {
+    Object.keys(colors).map(key =>
+        html.style.setProperty(transformKey(key), colors[key])
+    )
+}
+
+
+checkbox.addEventListener("change", ({
+    target
+}) => {
+    target.checked ? changeColors(darkMode) : changeColors(initialColors)
+})
